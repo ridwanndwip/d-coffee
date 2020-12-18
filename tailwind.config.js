80c605b3233f1a252644b2e4e93d99492255ec1c
ridwanndwip <ridwandwip.rpl2@gmail.com>
2020-12-18 21:55:05 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: { 
+      extend: {
+        colors: {
+          'color1': '#b58b5e',
+          'color2': '#211a13',
+          'color3': '#e59f15',
+          'color4': '#8d8986'
+        },
+      fontFamily: {
+        'poppins': ['Poppins'],
+      },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
